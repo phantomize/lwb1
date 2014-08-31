0de5825c305225e3de72a421b69cd2a635b26f06
Josh Brake <joshua.brake@gmail.com>
2014-08-31 17:22:39 +1200

    testing build

--- gpsHandler.js
+++ gpsHandler.js
@@ -18,14 +18,20 @@
 //   the current GPS coordinates
 //
 var onSuccess = function(position) {
-    alert('Latitude: '          + position.coords.latitude          + '\n' +
-          'Longitude: '         + position.coords.longitude         + '\n' +
-          'Altitude: '          + position.coords.altitude          + '\n' +
-          'Accuracy: '          + position.coords.accuracy          + '\n' +
-          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-          'Heading: '           + position.coords.heading           + '\n' +
-          'Speed: '             + position.coords.speed             + '\n' +
-          'Timestamp: '         + position.timestamp                + '\n');
+//    alert('Latitude: '          + position.coords.latitude          + '\n' +
+//          'Longitude: '         + position.coords.longitude         + '\n' +
+//          'Altitude: '          + position.coords.altitude          + '\n' +
+//          'Accuracy: '          + position.coords.accuracy          + '\n' +
+//          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+//          'Heading: '           + position.coords.heading           + '\n' +
+//          'Speed: '             + position.coords.speed             + '\n' +
+//          'Timestamp: '         + position.timestamp                + '\n');
+
+    var vars = [position.coords.latitude, position.coords.longitude, position.timestamp]
+
+    alert(vars[0] + ", " + vars[1] + ", " + vars[2])
+
+    //return vars
 };
 
 // onError Callback receives a PositionError object
